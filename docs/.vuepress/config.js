62d22666093d6711c7b8d88272296beffb75b1c8
yo1000 <yoichi.kikuchi.aug@gmail.com>
2018-06-08 02:23:51 +0900

    Add favicon settings

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,14 @@ module.exports = {
   base: '/',
   ga: 'UA-119554407-1',
   head: [
+    ['link', { rel: 'shortcut icon', type: 'image/x-icon', href: '/favicon/favicon.ico' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-16x16.png' }],
+    ['link', { rel: 'manifest', type: 'image/png', href: '/favicon/site.webmanifest' }],
+    ['link', { rel: 'mask-icon', type: 'image/png', href: '/favicon/safari-pinned-tab.svg', color: '#5bbad5' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#da532c' }],
+    ['meta', { name: 'theme-color', content: '#ffffff' }],
     ['link', { rel: 'alternate', type: 'application/rss+xml', href: '/rss.xml', title: 'talk2wall | latest posts feed' }]
   ],
   sidebar: 'auto',
